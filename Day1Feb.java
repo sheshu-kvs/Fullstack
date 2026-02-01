d4a5fae8a401e87d0c7d379af412a83503bc2ffd
Sheshadri <kvs24161@gmail.com>
2026-02-01 22:53:26 +0530

    Remove Special Character

--- Day1Feb.java
+++ Day1Feb.java
@@ -1,7 +1,16 @@
 public class Day1Feb {
-   
+    static void removeSpecialCharacter(String str){
+        String t="";
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(!Character.isLetter(ch)){
+              t=t+ch;
+            }
+        }
+        System.out.println(t);
+    }
     public static void main(String[] args) {
-      
+        FindDuplicates("heleo!2#$");
     }
     
 }
