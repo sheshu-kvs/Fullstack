29b55728c18c073c5ed1b97b8cbfc017fef1d7f3
Sheshadri <kvs24161@gmail.com>
2026-02-08 23:46:02 +0530

    Reverse Number

---
+++ Day8Feb.java
@@ -0,0 +1,15 @@
+public class Day8Feb {
+    static void ReverNumber(){
+        int num=123;
+        int rev=0;
+        while(num>0){
+            int rem = num%10;
+            rev = rev *10+rem;
+            num=num/10;
+        }
+        System.out.println("Reverse Number "+rev);
+    }
+    public static void main(String[] args) {
+        ReverNumber();
+    }
+}
